b80c05c9d1ed75634ad398128fea124d921af913
smak <samuel_mak2@outlook.com>
2025-09-02 16:35:06 +0100

    Added DPI and other GNOME native configs and now desktop file pointing to the actual sh rather than the symlink

--- scripts/xresources.sh
+++ scripts/xresources.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
-# Tried pointing to projects/setup/config/.Xresources and it didn't work
-# I think x applications look for the .Xresources in the home directory
-# So I've made a symlink in home called .Xresources pointing to 
-# projects/setup/config/.Xresources
-xrdb -merge  /home/smak/.Xresources
+xrdb -merge /home/smak/projects/setup/config/.Xresources
 
--- setup.sh
+++ setup.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/env bash
 
-# Create symlink to .Xresources
-if [ ! -f "~/.Xresources" ]; then
-	ln -s "~/projects/setup/config/.Xresources" "~/.Xresources"
-fi
-
 # Create desktop application autostart config script
-if [ ! -d "~/.config/autostart" ]; then
-	cd "~/.config"
-	mkdir "autostart"
+if [ ! -d ~/.config/autostart ]; then
+	cd ~/.config
+	mkdir autostart
 fi 
-cd "~/.config/autostart"
-cp "~/projects/setup/startup/xresources.desktop" .
+cd ~/.config/autostart
+cp ~/projects/setup/startup/xresources.desktop .
 
