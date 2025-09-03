5b0ee75aab8ff9147eb70595ca01807730524f65
smak <samuel_mak2@outlook.com>
2025-09-03 14:49:33 +0100

    xrdb .Xresources
    Tried sleeping 1 and 2 seconds but didn't work, need at least 3 seconds
    for DISPLAY to be loaded before sourcing

--- scripts/xresources.sh
+++ scripts/xresources.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+# Need to wait for the DISPLAY variable to be defined before sourcing (currently adding sleep of 3 seconds, tried 1 and 2 seconds but didn't work))
+ 
 (sleep 3 && xrdb -merge /home/smak/projects/setup/config/.Xresources) &
 
