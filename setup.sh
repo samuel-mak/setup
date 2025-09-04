6f698692ea36ca26aeab586c1051d8bf15805719
smak <samuel_mak2@outlook.com>
2025-09-04 02:29:32 +0100

    Set wallpaper to solid black

--- setup.sh
+++ setup.sh
@@ -11,3 +11,10 @@ cp ~/projects/setup/startup/xresources.desktop .
 
 # Set UK keyboard (replace 'gb' with 'us' for US layout)
 gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'gb')]"
+
+# Remove wallpaper (which results in solid black wallpaper - prefer this method to creating
+# a black png file
+# The line below should remove any pictures and should change to a solid colour
+# But the solid colour may not be black, so the second line sets the colour to black
+gsettings set org.gnome.desktop.background picture-options 'none'
+gsettings set org.gnome.desktop.background primary-color '#000000'
