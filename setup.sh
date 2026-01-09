e00e033a2168bf91ee03a5a5f2d00f1ed0abfcd2
smak <samuel_mak@outlook.com>
2026-01-09 19:12:51 +0000

    Updated setup.sh
    Added gsettings for alt tab show window and set it to show all
    opened windows and not group windows by applications as set by
    default

--- setup.sh
+++ setup.sh
@@ -80,6 +80,8 @@ gsettings set org.gnome.desktop.interface enable-hot-corners false
 # Time format
 gsettings set org.gnome.desktop.interface clock-format '24h'
 
+# Show all windows
+gsettings set org.gnome.desktop.wm.keybindings switch-applications "['<Super>Tab', '<Alt>Tab']"
 
 # MacBook Pro Intel/T2 specific settings
 # Disable automatic suspend when plugged in to power source
